f938cca569347f50f69e4c1b8e8527b21e57f5af
芷炫 <lvshuncai@gmail.com>
2019-08-04 05:04:30 +0800

    更新Readme
    Signed-off-by: 芷炫 <lvshuncai@gmail.com>

--- README.md
+++ README.md
@@ -1,31 +1,83 @@
 # QQ空间导出助手
 
-> QQ空间导出助手，用于备份QQ空间的说说、相册、视屏、日志、留言板为文件，供永久保存
+> QQ空间导出助手，用于备份QQ空间的说说、相册、视频、日志、留言板为文件，供永久保存
 
 ## 简介
 
 互联网产品都有着自己的生命周期，新浪博客相册，网易相册，QQ账号注销等等，于是写了这个基于Chrome的QQ空间导出助手的谷歌扩展，用于备份QQ空间的日志、说说、相册、留言板为文件，供永久保存。
 
 ## 安装
+#### 源码安装
 - 下载源码
 - 打开[谷歌扩展](chrome://extensions/)
 - 勾选开发者模式
 - 点击[加载已解压的扩展程序]按钮
 - 选择QZoneExport/src文件夹
 
+#### Google
+- [QQ空间导出助手](https://atom.io/)
+
 
 ## 使用
-- 登录QQ空间
+- 登录QQ空间  
+  支持空间URL：https://user.qzone.qq.com/QQ号  
+  不支持空间URL：https://QQ号.qzone.qq.com
 - 右上角点击插件图标  
- ![](https://i.loli.net/2019/06/01/5cf254132b3fc11683.png)
+ ![](https://i.loli.net/2019/08/04/Td5hUAfcvH14Yik.png)
 - 勾选备份内容
 - 点击开始备份
 - 等待备份完成   
-![](https://i.loli.net/2019/06/01/5cf254e31d29367825.png)
+![](https://i.loli.net/2019/08/04/wiIQYvGEsR38Z12.png)
 - 点击下载备份  
-![](https://i.loli.net/2019/06/01/5cf25514c02d929280.png)
-- 保存
+![](https://i.loli.net/2019/08/04/LSoxmFJAf4Ms9NZ.png)
+- 下载并保存  
+![](https://i.loli.net/2019/08/04/NIkosuxiRKLHlFB.png)
 - 推荐使用 [Atom](https://atom.io/) Markdown编辑器查看.md备份内容
+- 备份目录结构如下    
+    ├─好友  
+    │  └─QQ好友.xlsx  
+    ├─日志  
+    │  ├─images  
+    │  │  └─图片名称  
+    │  └─分类名称  
+    │      └─日志标题.md  
+    ├─留言板  
+    │  └─留言板.md  
+    ├─相册  
+    │  └─分类名称  
+    │      └─相册名称  
+    │          └─相片名称  
+    ├─私密日记  
+    │  ├─images  
+    │  │  └─图片名称  
+    │  └─日志标题.md  
+    ├─视频  
+    │  └─视频链接.downlist  
+    └─说说  
+    │   ├─2019年.md  
+    │   └─images  
+    │       └─图片名称  
+    ├─说明.md  
+
+## 备份内容预览
+### 说说
+![](https://s2.ax1x.com/2019/08/04/eySxun.png)
+### 日志
+![](https://s2.ax1x.com/2019/08/04/eySjjs.png)
+### 留言板
+![](https://i.loli.net/2019/08/04/3oj4emPFhC29iAQ.png)
+### 好友
+![](https://i.loli.net/2019/08/04/AhQB1PeGfTFl3N2.png)
+
+
+
+## 配置
+- 右键点击插件图标
+- 弹出菜单选择【选项】  
+- 配置页面可配置备份方式和选项
+![](https://i.loli.net/2019/08/04/XcqReS1sHInlCMN.png)
+- 推荐使用默认配置，遇到QQ冻结时，可调整查询间隔
+- 无法更改的配置表示尚未支持
 
 
 ## TO-DO
@@ -37,7 +89,5 @@
 - [x] 留言板导出
 - [x] 优化插件UI
 - [x] 原图相册导出
-- [ ] 视频导出
-- [ ] 视频说说导出
-- [ ] 音频说说导出
-- [ ] 音乐分享导出
+- [x] 视频导出
+- [ ] JSON导出
--- src/js/content-script.js
+++ src/js/content-script.js
@@ -69,51 +69,100 @@ const MODAL_HTML = `
 `
 
 const README_TEXT = `
-目录格式：
+# QQ空间导出助手
 
-QQ空间备份
-|--- 说明.txt
-└--- 日志
-    └--- images
-        |--- 图片A
-        |--- 图片B
-        └---- ....
-    |--- 日志A.md
-    |--- 日志B.md
-    |--- ...
-└--- 私密日记
-    └--- images
-        |--- 图片A
-        |--- 图片B
-        └---- ....
-    |--- 私密日记A.md
-    |--- 私密日记B.md
-    |--- ...
-└--- 相册
-    └--- 相册A
-        |--- 图片A
-        |--- 图片B
-        └---- ....
-└--- 视频
-    └--- 视频链接.txt
-└--- 说说
-    └--- images
-        |--- 图片A
-        |--- 图片B
-        └---- ....
-    └--- 2019.md
-    └--- 2018.md
-    └---- ....
-└--- 留言板
-    └--- images
-        |--- 图片A
-        |--- 图片B
-        └---- ....
-    └--- 留言板.md
-└--- 好友
-    └--- 好友.xlsx
+> QQ空间导出助手，用于备份QQ空间的说说、日志、私密日记、相册、视频、留言板、QQ好友为文件，供永久保存
 
-推荐使用 [Atom](https://atom.io/)
+## 简介
+
+互联网产品都有着自己的生命周期，新浪博客相册，网易相册，QQ账号注销等等，于是写了这个基于Chrome的QQ空间导出助手的谷歌扩展，用于备份QQ空间的说说、日志、私密日记、相册、视频、留言板、QQ好友为文件，供永久保存。
+
+## 安装
+#### 源码安装
+- 下载源码
+- 打开[谷歌扩展](chrome://extensions/)
+- 勾选开发者模式
+- 点击[加载已解压的扩展程序]按钮
+- 选择QZoneExport/src文件夹
+
+#### Google
+- [QQ空间导出助手](https://atom.io/)
+
+
+## 使用
+- 登录QQ空间  
+  支持空间URL：https://user.qzone.qq.com/QQ号  
+  不支持空间URL：https://QQ号.qzone.qq.com
+- 右上角点击插件图标  
+ ![](https://i.loli.net/2019/08/04/Td5hUAfcvH14Yik.png)
+- 勾选备份内容
+- 点击开始备份
+- 等待备份完成   
+![](https://i.loli.net/2019/08/04/wiIQYvGEsR38Z12.png)
+- 点击下载备份  
+![](https://i.loli.net/2019/08/04/LSoxmFJAf4Ms9NZ.png)
+- 下载并保存  
+![](https://i.loli.net/2019/08/04/NIkosuxiRKLHlFB.png)
+- 推荐使用 [Atom](https://atom.io/) Markdown编辑器查看.md备份内容
+- 备份目录结构如下    
+    ├─好友  
+    │  └─QQ好友.xlsx  
+    ├─日志  
+    │  ├─images  
+    │  │  └─图片名称  
+    │  └─分类名称  
+    │      └─日志标题.md  
+    ├─留言板  
+    │  └─留言板.md  
+    ├─相册  
+    │  └─分类名称  
+    │      └─相册名称  
+    │          └─相片名称  
+    ├─私密日记  
+    │  ├─images  
+    │  │  └─图片名称  
+    │  └─日志标题.md  
+    ├─视频  
+    │  └─视频链接.downlist  
+    └─说说  
+    │   ├─2019年.md  
+    │   └─images  
+    │       └─图片名称  
+    ├─说明.md  
+
+## 备份内容预览
+### 说说
+![](https://s2.ax1x.com/2019/08/04/eySxun.png)
+### 日志
+![](https://s2.ax1x.com/2019/08/04/eySjjs.png)
+### 留言板
+![](https://i.loli.net/2019/08/04/3oj4emPFhC29iAQ.png)
+### 好友
+![](https://i.loli.net/2019/08/04/AhQB1PeGfTFl3N2.png)
+
+
+
+## 配置
+- 右键点击插件图标
+- 弹出菜单选择【选项】  
+- 配置页面可配置备份方式和选项
+![](https://i.loli.net/2019/08/04/XcqReS1sHInlCMN.png)
+- 推荐使用默认配置，遇到QQ冻结时，可调整查询间隔
+- 无法更改的配置表示尚未支持
+
+
+## TO-DO
+- [x] 普通日志导出
+- [x] 私密日记导出
+- [x] 普通说说导出
+- [x] QQ好友导出
+- [x] 普通相册导出
+- [x] 留言板导出
+- [x] 优化插件UI
+- [x] 原图相册导出
+- [x] 视频导出
+- [ ] JSON导出
+
 `
 
 /**
@@ -683,7 +732,7 @@ async function initFolder() {
 
         console.info("开始创建说明文件");
         // 创建说明文件
-        QZone.Common.Filer.write(FOLDER_ROOT + "说明.txt", { data: README_TEXT, type: "text/plain" }, (entry) => {
+        QZone.Common.Filer.write(FOLDER_ROOT + "说明.md", { data: README_TEXT, type: "text/plain" }, (entry) => {
             console.info('创建文件成功：' + entry.fullPath);
             return true;
         });
